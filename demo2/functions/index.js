5cbf039f5a9097055b06d689e69ca606a42369e8
Jerry Jalava <jerry.jalava@qvik.fi>
2017-06-20 16:19:29 +0300

    removed debug testing

--- demo2/functions/index.js
+++ demo2/functions/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-require('@google-cloud/trace-agent').start();
-
 exports.helloPubSub = function (event, callback) {
   const pubsubMessage = event.data;
   const name = pubsubMessage.data ? Buffer.from(pubsubMessage.data, 'base64').toString() : 'World';
