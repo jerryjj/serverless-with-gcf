8cc89584fcb9f5b3d1fa781be5ea62e5a0d74776
Jerry Jalava <jerry.jalava@qvik.fi>
2017-06-20 16:08:01 +0300

    testing debugging

--- demo2/functions/index.js
+++ demo2/functions/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('@google-cloud/debug-agent').start();
+
 exports.helloPubSub = function (event, callback) {
   const pubsubMessage = event.data;
   const name = pubsubMessage.data ? Buffer.from(pubsubMessage.data, 'base64').toString() : 'World';
